3faab64a2642d65a15b61b8a4f9111e7ceeeca1a
Amel Hasić <amelhasic23@outlook.com>
2025-03-03 09:35:28 +0100

    Commit

---
+++ MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace CardLayout;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder.UseMauiApp<App>();
+
+		builder.Services.AddSingleton<MainPageViewModel>();
+		builder.Services.AddSingleton<MainPage>();
+
+		return builder.Build();
+	}
+}
